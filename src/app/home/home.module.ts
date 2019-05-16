b6eb94fc20afc25f09815ddb7e77c8040c905858
Flavian-Calinic Stuparu <flavian.stuparu@gmail.com>
2019-05-16 22:37:04 +0300

    fix(Schedule): improve donation center & date-time selection form

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptFormsModule } from "nativescript-angular";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NativeScriptDateTimePickerModule } from "nativescript-datetimepicker/angular";
+import { NativeScriptPickerModule } from "nativescript-picker/angular";
 
 import { ChecklistComponent } from "./checklist/checklist.component";
 import { HomeRoutingModule  } from "./home-routing.module";
@@ -19,6 +21,8 @@ import { ScheduleComponent } from "./schedule/schedule.component";
         NativeScriptCommonModule,
         HomeRoutingModule,
         NativeScriptFormsModule,
+        NativeScriptDateTimePickerModule,
+        NativeScriptPickerModule,
     ],
     providers: [
     ],
